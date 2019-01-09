9930f967a1889c83f581eec297da2469c8fec8ec
Luis Raygadas <lraygada@pnw.edu>
2019-01-08 23:46:13 -0600

    Uploading Current project, with new toggle to mute music

---
+++ Unity UI Project/Assets/Scripts/Change_Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_Scene : MonoBehaviour
+{
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+		Application.LoadLevel(sceneToChangeTo);
+ 		       
+    }
+}
